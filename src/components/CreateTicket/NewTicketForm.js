1b547c65f013adc5cc90886f1ea548097460d5e7
Lavesh Rawal <officialrawallavesh@gmail.com>
2023-01-26 15:40:30 +0530

    updated Create request form

--- src/components/CreateTicket/NewTicketForm.js
+++ src/components/CreateTicket/NewTicketForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "./style.css"
 
 function TicketForm() {
   const [ticket, setTicket] = useState({
@@ -27,15 +28,97 @@ function TicketForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Title:
-        <input
-          type="text"
-          name="title"
-          value={ticket.title}
-          onChange={handleChange}
-        />
-      </label>
+    <label>
+    Requester:
+    <select
+      name="Requester"
+      value={ticket.Requester}
+      onChange={handleChange}
+    >
+      <option value="administrator">administrator</option>
+      <option value="medium">Guest</option>
+      <option value="Guest">Lavesh Rawal</option>
+      <option value="Rupal Deshmukh">Rupal Deshmukh</option>
+      <option value="Vikramjeet">Vikramjeet Bhehra</option>
+      <option value="Saurabh Binsnoi">Saurabh Binsnoi</option>
+    </select>
+  </label>
+
+  <label>
+  Request Type:
+  <select
+    name="RequestType"
+    value={ticket.RequestType}
+    onChange={handleChange}
+  >
+    <option value="Not Specified">Not Specified</option>
+    <option value="Incedent">Incedent</option>
+    <option value="RequestInformation">Request Information</option>
+      </select>
+</label>
+
+<label>
+Status:
+<select
+  name="status"
+  value={ticket.status}
+  onChange={handleChange}
+>
+  <option value="open">Open</option>
+  <option value="closed">Closed</option>
+  <option value="wfc">waiting for customer</option>
+  <option value="hold">on hold</option>
+  <option value="PIA">Pending for Internal Action</option>
+</select>
+</label>
+
+<label>
+Mode:
+<select
+  name="Mode"
+  value={ticket.Mode}
+  onChange={handleChange}
+>
+  <option value="NotSpecified">Not Specified</option>
+  <option value="chat">chat</option>
+  <option value="email">email</option>
+  <option value="mobile">mobile</option>
+  <option value="webform">web form</option>
+</select>
+</label>
+
+<label>
+Group:
+<select
+  name="Group"
+  value={ticket.Group}
+  onChange={handleChange}
+>
+  <option value="NotSpecified">Not Specified</option>
+  <option value="HP">Hardware Problems</option>
+  <option value="NP">Network Problems</option>
+  <option value="LP">Laptop/Desktop Problem</option>
+  <option value="PP">Printer Problems</option>
+</select>
+</label>
+
+<label>
+Technician:
+<select
+  name="Mode"
+  value={ticket.Technician}
+  onChange={handleChange}
+>
+  <option value="NotSpecified">Not Specified</option>
+  <option value="administrator">administrator</option>
+  <option value="medium">Guest</option>
+  <option value="Guest">Lavesh Rawal</option>
+  <option value="Rupal Deshmukh">Rupal Deshmukh</option>
+  <option value="Vikramjeet">Vikramjeet Bhehra</option>
+  <option value="Saurabh Binsnoi">Saurabh Binsnoi</option>
+</select>
+</label>
+
       <br />
       <label>
         Description:
@@ -46,17 +129,7 @@ function TicketForm() {
         />
       </label>
       <br />
-      <label>
-        Status:
-        <select
-          name="status"
-          value={ticket.status}
-          onChange={handleChange}
-        >
-          <option value="open">Open</option>
-          <option value="closed">Closed</option>
-        </select>
-      </label>
+      
       <br />
       <label>
         Created At:
@@ -88,6 +161,7 @@ function TicketForm() {
           <option value="low">Low</option>
           <option value="medium">Medium</option>
           <option value="high">High</option>
+          <option value="critical">critical</option>
         </select>
       </label>
       <br />
